0bb165979e44967f0a7de4a653b41976e6005f17
Hirasamad <157278043+Hirasamad@users.noreply.github.com>
2024-05-25 23:50:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+let username = "Hira";
+alert(username);
+let message = "Hello World";
+alert(message)
+
+let x = "Hira"
+alert(x)
+let y = 24
+
+alert(y + " years old")
+let z = "Certified Mobile Application Development"
+alert(z)
+
+alert("PIZZA \nPIZZ\nPIZ\nPI")
+
+let email = "hirah5071@gmail.com"
+alert("My email is " + email)
+
+let book = "A smarter way to learn JavaScript."
+alert("I am trying to learning from a book" + book)
+document.write("Yah! I can write HTML content through Javascript")
+let a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(a)
